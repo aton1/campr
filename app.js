18f6809c725bad95ddf087b4dd64cad694625f7b
aton1 <anthonylton1@gmail.com>
2020-05-18 00:38:36 -0700

    created header and footer partials, added bootstrap, created form and added post route, installed body-parser

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) =>{
+const campgrounds = [
+  {name: "Salmon Creek", image:"https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e5074417c287ad79448cd_340.jpg"},
+  {name: "Yosemite", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417c287ad79448cd_340.jpg"},
+  {name: "Big Bear Lake", image: "https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e5074417c287ad79448cd_340.jpg"}
+];
+
+app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-  const campgrounds = [
-    {name: "Salmon Creek", image:"https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e5074417c287ad79448cd_340.jpg"},
-    {name: "Yosemite", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417c287ad79448cd_340.jpg"},
-    {name: "Big Bear Lake", image: "https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e5074417c287ad79448cd_340.jpg"}
-  ]
-
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/campgrounds", (req, res) => {
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {name: name, image: image}
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
 });
 
 app.listen(3000, () => {
